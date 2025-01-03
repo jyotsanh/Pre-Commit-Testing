e08a7526072b9933555c86d4722db1042fd96223
jyotsanh <hamaljyotsan@gmail.com>
2025-01-03 17:54:55 +0545

    neeee

---
+++ my_project/__init__.py
@@ -0,0 +1,6 @@
+# my_project/__init__.py
+
+
+def add(a, b):
+    """Function to add two numbers."""
+    return a + b
--- pytest/test_example.py
+++ pytest/test_example.py
@@ -1,10 +1,28 @@
+# tests/test_case.py
+import sys
+import os
+
+# Import statements should be placed at the top
+from my_project import add
+
+# Add the root directory to the path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
+
+
+# Test functions
 def test_always_passes():
     assert True, "working fine"
 
 
 def test_always_fails():
-    assert True, "working fine"
+    assert True, "this test will fail"
 
 
 def test_looks_great():
-    assert True
+    assert True  # This test is trivially passing
+
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+    assert add(0, 0) == 0
